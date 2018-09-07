a46f8d9ecb42115b7cf51fe9ef6d05df4462a3cb
Maria Budzanowska <maria.budzanowska@capgemini.com>
2018-09-07 14:50:07 +0200

    Using a Third-Party Component

--- indecision-app/src/components/Action.js
+++ indecision-app/src/components/Action.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export default (props) => {
-    return (
-      <div>
-        <button 
-          onClick={props.handlePick}
-          disabled={!props.hasOptions}>What should I do?</button>
-      </div>
-    );
-  }
+export default (props) =>  
+(
+  <div>
+    <button 
+      onClick={props.handlePick}
+      disabled={!props.hasOptions}>What should I do?</button>
+  </div>
+);
+  
--- indecision-app/src/components/IndecisionApp.js
+++ indecision-app/src/components/IndecisionApp.js
@@ -3,10 +3,12 @@ import AddOption from './AddOptions';
 import Options from './Options'
 import Header from './Header'
 import Action from './Action'
+import OptionModal from './OptionModal'
 
 export default class IndecisionApp extends React.Component {
   state = {
-    options: []
+    options: [],
+    selectedOption: undefined
   };
 
   handleDeleteOptions = () => {
@@ -19,10 +21,14 @@ export default class IndecisionApp extends React.Component {
     }));
   }
 
+  handleClearSelectedOption = () => {
+    this.setState(() => ({selectedOption: undefined}))
+  }
+
   handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
-    alert(option);
+    this.setState(() => ({selectedOption: option}))
   }
 
   handleAddOption = (option) => {
@@ -78,6 +84,9 @@ export default class IndecisionApp extends React.Component {
         <AddOption
           handleAddOption={this.handleAddOption}
         />
+        <OptionModal 
+          selectedOption={this.state.selectedOption}
+          handleClearSelectedOption={this.handleClearSelectedOption}/>
       </div>
     );
   }
