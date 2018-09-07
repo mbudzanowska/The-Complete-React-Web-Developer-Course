01c8bbbd52eb6f8c809db81d2d570b51676a90cc
Maria Budzanowska <m.budzanowska@orange.pl>
2018-09-07 21:15:05 +0200

    styling react

--- indecision-app/src/components/Option.js
+++ indecision-app/src/components/Option.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default (props) =>  
 (
-  <div>
-    {props.optionText}
-    <button onClick={(e) => {props.handleDeleteOption(props.optionText)}}>
+    <div className="option">
+      <p className="option__text" >{props.count}. {props.optionText}</p>
+    <button className="button button--link" onClick={(e) => {props.handleDeleteOption(props.optionText)}}>
       remove
     </button>
   </div>
--- indecision-app/src/components/Options.js
+++ indecision-app/src/components/Options.js
@@ -4,14 +4,20 @@ import Option from './Option'
 export default (props) => 
 (
   <div>
-    <button 
-      onClick={props.handleDeleteOptions}>Remove All
-    </button>
-    {props.options.length === 0 && <p>Please add an option to get started!</p>}
+  <div className="widget-header">
+        <h3 className="widget-header__title">Your Options</h3>
+      <button className="button button--link"
+        onClick={props.handleDeleteOptions}>Remove All
+      </button>
+
+  </div>
+  
+      {props.options.length === 0 && <p className="widget-message">Please add an option to get started!</p>}
     {
-      props.options.map((option) => (
+      props.options.map((option, index) => (
         <Option 
           key={option} 
+          count={index +1}
           optionText={option}
           handleDeleteOption={props.handleDeleteOption}
           />
