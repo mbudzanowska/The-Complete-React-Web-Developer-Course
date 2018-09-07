9f3d96e7169d3c8f7cfd27bf13af51f4e4426b88
Maria Budzanowska <maria.budzanowska@capgemini.com>
2018-09-07 14:09:42 +0200

    webpack

---
+++ indecision-app/src/components/Option.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default (props) => {
+    return (
+      <div>
+        {props.optionText}
+        <button onClick={(e) => {props.handleDeleteOption(props.optionText)}}>
+          remove
+        </button>
+      </div>
+    );
+  }
+    
